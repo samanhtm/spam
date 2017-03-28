3499b95a7c87bc5f861eeffa9010dbf9c80143b5
samanhtm <samanhatami997800@gmail.com>
2017-03-28 13:59:36 +0430

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+COUNTER=1
+while(true) do
+./beyond.sh
+curl "https://api.telegram.org/bot357240650:AAE7NWBJllh3c6jYCpWi9vqtN3ACHLD8FLc/sendmessage" -F "chat_id=185906807" -F "text=#NEWCRASH-#TeleBeyond-Reloaded-${COUNTER}-times"
+let COUNTER=COUNTER+1
+done
+ 
